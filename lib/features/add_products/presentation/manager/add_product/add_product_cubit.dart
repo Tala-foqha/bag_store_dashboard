faf3163250a29237d06b1190cd6896cbbde30a5d
tala <foqhat835@gmail.com>
2025-06-28 22:00:09 +0300

    add products cubit done

---
+++ lib/features/add_products/presentation/manager/add_product/add_product_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_product_state.dart';
+
+class AddProductCubit extends Cubit<AddProductState> {
+  AddProductCubit() : super(AddProductInitial());
+
+  
+}
---
+++ lib/features/add_products/presentation/manager/add_product/add_product_state.dart
@@ -0,0 +1,8 @@
+part of 'add_product_cubit.dart';
+
+@immutable
+abstract class AddProductState {}
+
+class AddProductInitial extends AddProductState {}
+
+
